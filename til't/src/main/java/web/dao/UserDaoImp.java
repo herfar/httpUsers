a466148fc98b32be9818efa55e45cd3dfa52fee5
herfar <94548763+herfar@users.noreply.github.com>
2022-11-17 22:00:12 +0300

    Update UserDaoImp.java

--- til't/src/main/java/web/dao/UserDaoImp.java
+++ til't/src/main/java/web/dao/UserDaoImp.java
@@ -16,34 +16,25 @@ public class UserDaoImp implements UserDao {
     EntityManager entityManager;
 
     @Override
-    @Transactional
     public List<User> informationOfPeoples() {
         TypedQuery<User> query = entityManager.createQuery("FROM User", User.class);
         return query.getResultList();
     }
-
     @Override
-    @Transactional
     public User getUserById(int id) {
         return entityManager.find(User.class, id);
     }
-
     @Override
-    @Transactional
     public void addUser(User user) {
         entityManager.persist(user);
     }
-
     @Override
-    @Transactional
     public User updateUser(int id, User user) {
         User user1 = entityManager.find(User.class, id);
         user1.setName(user.getName());
         return entityManager.merge(user1);
     }
-
     @Override
-    @Transactional
     public void removeUser(int id) {
         User user = entityManager.find(User.class, id);
         entityManager.remove(user);
